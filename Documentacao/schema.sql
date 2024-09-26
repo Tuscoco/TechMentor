15828b421e9d170622127cbecc5da1fcd6a65d78
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2024-09-26 19:32:56 -0300

    Atualização do esquema SQL e do mapa conceitual

--- Documentacao/schema.sql
+++ Documentacao/schema.sql
@@ -20,13 +20,14 @@ CREATE TABLE materia_aluno (matricula INTEGER NOT NULL, cod_materia INTEGER NOT 
 CREATE TABLE evento (local VARCHAR(100) NOT NULL, data_hora VARCHAR(30), materia INTEGER NOT NULL, nome VARCHAR(50), PRIMARY KEY(local, data_hora), FOREIGN KEY(materia) REFERENCES materia(cod_materia) ON DELETE SET NULL);
 
 -- CRIAR TABELA REPOSITORIO
-CREATE TABLE repositorio (nome VARCHAR(50) NOT NULL, cod_materia INTEGER NOT NULL, PRIMARY KEY(nome), FOREIGN KEY(cod_materia) REFERENCES materia(cod_materia) ON DELETE SET NULL);
+CREATE TABLE repositorio (nome VARCHAR(50) NOT NULL, link VARCHAR(300) NOT NULL, descricao VARCHAR(300), cod_materia INTEGER NOT NULL, PRIMARY KEY(cod_materia), FOREIGN KEY(cod_materia) REFERENCES materia(cod_materia) ON DELETE SET NULL);
 
 -- CRIAR TABELA MONITOR
 CREATE TABLE monitor (matricula INTEGER NOT NULL, cod_materia INTEGER NOT NULL, PRIMARY KEY(matricula, cod_materia), FOREIGN KEY(matricula) REFERENCES aluno(matricula) ON DELETE CASCADE, FOREIGN KEY(cod_materia) REFERENCES materia(cod_materia) ON DELETE CASCADE);
 
 -- CRIAR TABELA ATENDIMENTO
-CREATE TABLE atendimento (matricula_a INTEGER NOT NULL, matricula_m INTEGER NOT NULL, horario VARCHAR(10) NOT NULL, data VARCHAR(20) NOT NULL, cod_materia INTEGER NOT NULL, descricao VARCHAR(100) NULL, PRIMARY KEY(matricula_a, matricula_m, horario, data, cod_materia), FOREIGN KEY(matricula_a) REFERENCES aluno(matricula), FOREIGN KEY(matricula_m) REFERENCES monitor(matricula), FOREIGN KEY(cod_materia) REFERENCES monitor(cod_materia));
+CREATE TABLE atendimento (matricula_a INTEGER NOT NULL, matricula_m INTEGER NOT NULL, horario VARCHAR(10) NOT NULL, data VARCHAR(20) NOT NULL, cod_materia INTEGER NOT NULL, tema_duvida VARCHAR(20) NOT NULL, descricao VARCHAR(100) NULL, duvida_sanada BOOLEAN NOT NULL, PRIMARY KEY(matricula_a, matricula_m, horario, data, cod_materia), FOREIGN KEY(matricula_a) REFERENCES aluno(matricula), FOREIGN KEY(matricula_m) REFERENCES monitor(matricula), FOREIGN KEY(cod_materia) REFERENCES monitor(cod_materia));
 
 -- CRIAR TABELA CARGA_HORARIA
 CREATE TABLE carga_horaria (dia_da_semana VARCHAR(10) NOT NULL, matricula INTEGER NOT NULL, cod_materia INTEGER NOT NULL, horario_entrada VARCHAR(20) NULL, horario_saida VARCHAR(20) NULL, PRIMARY KEY(dia_da_semana, matricula, cod_materia), FOREIGN KEY(matricula, cod_materia) REFERENCES monitor(matricula, cod_materia));
+
