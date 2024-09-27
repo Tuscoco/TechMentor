00277dcac852a173ea0029ff06c4dfdf15784fb1
cavalo <felipefenske05@gmail.com>
2024-09-27 17:31:26 -0300

    monitoria styles

---
+++ Codigo/target/classes/public/js/monitoriaStyle.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const checkbox = document.getElementById('toggle');
+    const gradeCSS = document.getElementById('grade-css');
+    const pilhaCSS = document.getElementById('pilha-css');
+
+    // Verifica o estado da checkbox e alterna os estilos CSS
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            // Se marcado, desabilita o "monitoriaGrade" e habilita "monitoriaPilha"
+            gradeCSS.disabled = true;
+            pilhaCSS.disabled = false;
+        } else {
+            // Se desmarcado, habilita "monitoriaGrade" e desabilita "monitoriaPilha"
+            gradeCSS.disabled = false;
+            pilhaCSS.disabled = true;
+        }
+    });
+});
