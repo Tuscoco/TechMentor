4caec0f39d4871711075618ff016e4df622af72c
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2024-10-16 17:30:24 -0300

    Code att

--- Codigo/src/main/java/dao/AtendimentoDAO.java
+++ Codigo/src/main/java/dao/AtendimentoDAO.java
@@ -1,6 +1,4 @@
 package dao;
-<<<<<<< HEAD
-=======
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,8 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
->>>>>>> refs/remotes/origin/master
 import model.Atendimento;
 
 public class AtendimentoDAO {
