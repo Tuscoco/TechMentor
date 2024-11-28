dd1195722d79a47c821b72b973d9386f7a006c4f
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2024-11-28 09:45:55 -0300

    Code att

--- Codigo/target/classes/public/js/att.js
+++ Codigo/target/classes/public/js/att.js
@@ -76,9 +76,18 @@ function criarBotaoMonitor(monitor, materia, pontosTotais, email, foto, atendime
                         <p class="pontosMes">Pontos Mensais: </p>
                         <select class="mes">
                             <option default selected disabled class="disabled" style="display: none;">Selecione um mês</option>
-                            <option value="1">Janeiro</option>
-                            <option value="2">Fevereiro</option>
-                            <option value="12">Dezembro</option>
+                                <option value="1">Janeiro</option>
+                                <option value="2">Fevereiro</option>
+                                <option value="3">Março</option>
+                                <option value="4">Abril</option>
+                                <option value="5">Maio</option>
+                                <option value="6">Junho</option>
+                                <option value="7">Julho</option>
+                                <option value="8">Agosto</option>
+                                <option value="9">Setembro</option>
+                                <option value="10">Outubro</option>
+                                <option value="11">Novembro</option>
+                                <option value="12">Dezembro</option>
                         </select>
                         <button class="send">Ok</button>
                     </div>
